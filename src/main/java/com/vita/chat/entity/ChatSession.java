153592a50203169418499db0520df44d75a497bf
alsthdua107 <alsthdua107@gmail.com>
2026-09-16 15:17:29 +0900

    feat: 채팅 엔티티 생성

---
+++ src/main/java/com/vita/chat/entity/ChatSession.java
@@ -0,0 +1,41 @@
+package com.vita.chat.entity;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatSession {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+    private String title;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    
+    @Builder
+    public ChatSession(Long userId, String title){
+    	this.userId = userId;
+    	this.title = title;
+    }
+    
+    @PrePersist
+    void onCreate() {
+        this.createdAt = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+        this.updatedAt = this.createdAt;
+    }
+}
