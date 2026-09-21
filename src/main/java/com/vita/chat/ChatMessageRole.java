c1cf04c60716c0cef3f18efc8121aca9fdebb9eb
alsthdua107 <alsthdua107@gmail.com>
2026-09-21 13:27:26 +0900

    refactor(db): enum 값 대문자 통일
    - DB enum 값을 대문자로 통일
    - enum 직렬화/역직렬화 시 대소문자 변환 로직 제거
    - @JsonValue, @JsonCreator 기반 변환 메서드 삭제

--- src/main/java/com/vita/chat/ChatMessageRole.java
+++ src/main/java/com/vita/chat/ChatMessageRole.java
@@ -1,19 +1,6 @@
 package com.vita.chat;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
-
 public enum ChatMessageRole {
 	USER,
 	ASSISTANT;
-	
-	@JsonValue
-    public String toValue() {
-        return name().toLowerCase();
-    }
-
-    @JsonCreator
-    public static ChatMessageRole from(String value) {
-        return ChatMessageRole.valueOf(value.toUpperCase());
-    }
 }
