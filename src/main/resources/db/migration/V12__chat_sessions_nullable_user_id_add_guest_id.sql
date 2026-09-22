da0a094d6c9409e8aa5c532c1d04b46e5a5b916a
alsthdua107 <alsthdua107@gmail.com>
2026-09-22 17:10:41 +0900

    fix: V12 마이그레이션 CHECK문 세미콜론 추가

--- src/main/resources/db/migration/V12__chat_sessions_nullable_user_id_add_guest_id.sql
+++ src/main/resources/db/migration/V12__chat_sessions_nullable_user_id_add_guest_id.sql
@@ -7,6 +7,6 @@ ALTER TABLE chat_sessions ADD COLUMN guest_id UUID;
 -- user_id 와 guest_id 둘 다 없는 세션 방지
 ALTER TABLE chat_sessions
     ADD CONSTRAINT chk_chat_sessions_owner
-    CHECK ((user_id IS NULL) != (guest_id IS NULL))
+    CHECK ((user_id IS NULL) != (guest_id IS NULL));
 
  CREATE INDEX idx_chat_sessions_guest_id ON chat_sessions (guest_id);
